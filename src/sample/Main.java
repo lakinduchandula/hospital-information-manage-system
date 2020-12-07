6b4536df663b3fc2dfd0b808bc512104c52048d2
lakinduchandula <lakindu.chandula@hotmail.com>
2020-12-07 16:58:23 +0530

    Login interface style.css change

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view/AdminDashboard.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view/Login.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1050, 576));
         primaryStage.show();
