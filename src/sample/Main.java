d8a9d2d48136b2d8cff738da7904c6bc97b65e76
dashboard-patient <tharundi.dunuwila@gmail.com>
2020-12-09 20:33:08 +0530

    Added Appointment

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view/Login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view/Appointment.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1050, 576));
         primaryStage.show();
