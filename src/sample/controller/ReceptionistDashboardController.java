10e3b7bd6ae7c7cae098578a2e1c15c370055b9a
Sandaru Rashmitha <sandarurashmitha98@gmail.com>
2020-12-11 16:17:52 +0530

    Added Appointemnt

--- src/sample/controller/ReceptionistDashboardController.java
+++ src/sample/controller/ReceptionistDashboardController.java
@@ -2,6 +2,12 @@ package sample.controller;
 
 import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
 
 public class ReceptionistDashboardController {
 
@@ -22,5 +28,14 @@ public class ReceptionistDashboardController {
 
     @FXML
     private JFXButton RecepDashUserBtn;
+
+    @FXML
+    private BorderPane ReceptDashBoarderPane;
+
+    @FXML
+    void show_appointment(MouseEvent event) throws IOException {
+        Parent appointmentPane = FXMLLoader.load(getClass().getResource("/sample/view/ReceptDashAppointment.fxml"));
+        ReceptDashBoarderPane.setCenter(appointmentPane);
+    }
 
 }
