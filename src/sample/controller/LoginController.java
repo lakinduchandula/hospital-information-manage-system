95a2dd175f5608cc11b43f239445eb4e5f30e775
lakinduchandula <lakindu.chandula@hotmail.com>
2020-12-11 18:58:08 +0530

    working on loginController but commit this to pull the code

--- src/sample/controller/LoginController.java
+++ src/sample/controller/LoginController.java
@@ -4,6 +4,8 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 
 public class LoginController {
@@ -29,6 +31,34 @@ public class LoginController {
         LoginUsermod.getItems().add("Patient");
         LoginUsermod.getItems().add("Medical Officer");
         LoginUsermod.getItems().add("Admin");
+
+        LoginButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                if(!LoginUsername.getText().toString().trim().equals("") && !LoginPassword.getText().toString().trim().equals("")){
+                    switch(LoginUsermod.getValue()){
+                        case "Receptionist" : {
+
+                        }
+                        case "Patient" : {
+
+                        }
+                        case "Medical Officer" : {
+
+                        }
+                        case "Admin" : {
+
+                        }
+                        default : {
+
+                        }
+                    }
+                } else {
+                    System.out.println("Please Enter Login Credentials");
+                }
+
+            }
+        });
 
     }
 }
