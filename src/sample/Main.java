f6b537c99fbb09cca3790923bc4c4e3c066a7eb1
Sandaru Rashmitha <sandarurashmitha98@gmail.com>
2020-12-11 12:26:11 +0530

    added receptionist dashboard

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        Parent root = FXMLLoader.load(getClass().getResource("view/AdminDashboard.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view/ReceptionistDashboard.fxml"));
 
 
 
